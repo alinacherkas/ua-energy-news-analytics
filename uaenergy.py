4440e29456d7a4c5802fb4b7eb06fa6be96a930c
Alina Cherkas <51997505+alinacherkas@users.noreply.github.com>
2024-12-22 11:46:47 +0100

    style: format the script

--- uaenergy.py
+++ uaenergy.py
@@ -2,6 +2,7 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+
 def parse_article(article):
     """
     Parse article preview on newspage to extract title, link and time
@@ -16,14 +17,15 @@ def parse_article(article):
     title, link, time : Tuple
         a tuple containing title, link and time of the article
     """
-    
+
     title = article.a.text
     link = article.a.get("href")
     time = " ".join([x.text for x in article.find_all("span")])
-    
+
     return title, link, time
 
-def parse_news(link:str, use_date:bool = True):
+
+def parse_news(link: str, use_date: bool = True):
     """
     Parses all articles on a newspage for a given date to produce a structured dataframe. Uses parse_articles.
 
@@ -36,25 +38,28 @@ def parse_news(link:str, use_date:bool = True):
 
     Returns
     -------
-    df_articles: pd.DataFrame 
+    df_articles: pd.DataFrame
         a dataframe of parsed articles with key information
     """
     if use_date:
         link = f"https://ua-energy.org/uk/news?date={link}"
-        
-    #Obtaining .html page and checking the status code
+
+    # Obtaining .html page and checking the status code
     results = requests.get(link)
     assert results.status_code == 200, f"HTTP Error Code: {results.status_code}"
-    
-    #Locating news section and parsing the articles
+
+    # Locating news section and parsing the articles
     soup = BeautifulSoup(results.content, features="lxml")
     news_section = soup.find_all("div", {"class": "wrap"})[1]
     articles = news_section.find_all("div", {"class": "article"})
-    df_articles = pd.DataFrame([parse_article(x) for x in articles], columns=["Title", "Link", "Date"])
-    
+    df_articles = pd.DataFrame(
+        [parse_article(x) for x in articles], columns=["Title", "Link", "Date"]
+    )
+
     return df_articles
 
-def get_article_content(link:str, add_root:bool = True, warnings:bool = False):
+
+def get_article_content(link: str, add_root: bool = True, warnings: bool = False):
     """
     Parse a full article to extract basic information
 
@@ -74,34 +79,45 @@ def get_article_content(link:str, add_root:bool = True, warnings:bool = False):
     """
     if add_root:
         link = "https://ua-energy.org" + link
-        
+
     results = requests.get(link)
     assert results.status_code == 200, f"HTTP Error Code: {results.status_code}"
-        
+
     soup = BeautifulSoup(results.content, features="lxml")
     class_name = "col-lg-7 col-md-8 col-sm-8 col-lg-offset-2 content-article content-article-inner"
     article_object = soup.find("div", {"class": class_name})
     if article_object is None:
-        if warnings: print("No article found:", link)
+        if warnings:
+            print("No article found:", link)
         return link, None, None, None
-        
-    read_also = [tag.a.get("href") for tag in article_object.find_all("p")
-                 if "ЧИТАЙТЕ ТАКОЖ" in tag.text and tag.find('a') is not None]
-    if len(read_also) == 0: read_also = None
-    
-    article_text = [tag.text for tag in article_object.find_all("p") if not "ЧИТАЙТЕ ТАКОЖ" in tag.text]
+
+    read_also = [
+        tag.a.get("href")
+        for tag in article_object.find_all("p")
+        if "ЧИТАЙТЕ ТАКОЖ" in tag.text and tag.find("a") is not None
+    ]
+    if len(read_also) == 0:
+        read_also = None
+
+    article_text = [
+        tag.text
+        for tag in article_object.find_all("p")
+        if not "ЧИТАЙТЕ ТАКОЖ" in tag.text
+    ]
     article_text = " ".join(article_text)
-    
+
     try:
-        tags = article_object.find("div", {"class":"tags"})
+        tags = article_object.find("div", {"class": "tags"})
         tags = {tag.get("href"): tag.text for tag in tags.find_all("a")}
     except:
-        if warnings: print("No tags found:", link)
+        if warnings:
+            print("No tags found:", link)
         tags = None
-        
+
     return link, article_text, tags, read_also
 
-def replace_months(date:str):
+
+def replace_months(date: str):
     """
     Convert an original date string to a datetime object
 
@@ -115,19 +131,21 @@ def replace_months(date:str):
     pd.to_datetime(date) : Timestamp
         pandas timestamp object
     """
-    months = {"жовтня":"October",
-              "травня":"May",
-              "квітня":"April",
-              "листопада":"November",
-              "вересня":"September",
-              "березня":"March",
-              "серпня":"August",
-              "грудня":"December",
-              "липня":"July",
-              "лютого":"February",
-              "червня":"June",
-              "січня":"January"}
-    
+    months = {
+        "жовтня": "October",
+        "травня": "May",
+        "квітня": "April",
+        "листопада": "November",
+        "вересня": "September",
+        "березня": "March",
+        "серпня": "August",
+        "грудня": "December",
+        "липня": "July",
+        "лютого": "February",
+        "червня": "June",
+        "січня": "January",
+    }
+
     ukr_month = date.split(" ")[1]
     date = date.replace(ukr_month, months[ukr_month])
     return pd.to_datetime(date)
